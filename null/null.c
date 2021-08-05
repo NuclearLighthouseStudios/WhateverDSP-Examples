9b99c4e37d3ea3f02d2f8d7e19e0ce8f27e9497f
Kris Slyka <kris@slyka.net>
2021-08-05 11:48:05 +0200

    Add null example, the example that does nothing

---
+++ null/null.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <libwdsp.h>
+
+void wdsp_process(float in_buffer[NUM_STREAMS][BLOCK_SIZE], float out_buffer[NUM_STREAMS][BLOCK_SIZE])
+{
+	for (int i = 0; i < BLOCK_SIZE; i++)
+	{
+		float l_samp = in_buffer[0][i];
+		float r_samp = in_buffer[1][i];
+
+		out_buffer[0][i] = l_samp;
+		out_buffer[1][i] = r_samp;
+	}
+}
