88f665642b9a54fcd0b7a85037f012a5a22e4dc2
Kris Slyka <kris@slyka.net>
2023-05-29 14:08:23 +0200

    Add bypass setting to config to make null example run on FXDSP out of the box

--- null/null.c
+++ null/null.c
@@ -11,3 +11,13 @@ void wdsp_process(float *in_buffer[BLOCK_SIZE], float *out_buffer[BLOCK_SIZE])
 		out_buffer[1][i] = r_samp;
 	}
 }
+
+void wdsp_init(void)
+{
+	// This checks if the bypass setting was set in the config.ini for
+	// the board we're currently compiling for.
+#if CONFIG_EFFECT_BYPASS == true
+	io_digital_out(BYPASS_L, true);
+	io_digital_out(BYPASS_R, true);
+#endif
+}
